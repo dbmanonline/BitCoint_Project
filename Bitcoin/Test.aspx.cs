c1d2de3526a2f0abbb9afa2a452fe25ae620c543
dbmanonline <dbman.online@gmail.com>
2016-04-27 20:49:56 +0700

    bin, config, model

---
+++ Bitcoin/Test.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Bitcoin.Data.DTO;
+
+public partial class Test : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!IsPostBack)
+        {
+            BitcoinEntities db = new BitcoinEntities();
+
+            var query = db.Bid.Count();
+
+            Response.Write(query.ToString());
+        }
+    }
+}
