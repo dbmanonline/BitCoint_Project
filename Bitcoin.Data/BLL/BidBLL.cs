0d376f6f7d6aafdd29b1cc15a10a12b90f7076c7
Khiem Nguyen <khiemnvd@hotmail.com>
2016-04-29 01:00:46 +0700

    add code insert bid

--- Bitcoin.Data/BLL/BidBLL.cs
+++ Bitcoin.Data/BLL/BidBLL.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Bitcoin.Data.DAL;
+using Bitcoin.Data.DTO;
 
 namespace Bitcoin.Data.BLL
 {
-    class BidBLL
+    public class BidBLL
     {
+        private static readonly BidDAL BidDal = new BidDAL();
+
+        public void InsertBid(Bid bid)
+        {
+            BidDal.InsertBid(bid);
+        }
     }
 }
