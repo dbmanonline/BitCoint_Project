00efd3bd9b069c1140bce72c0d30839f5409e9ba
Khiem Nguyen <khiemnvd@hotmail.com>
2016-04-29 00:53:11 +0700

    add code insert bid

--- Bitcoin.Data/DAL/BidDAL.cs
+++ Bitcoin.Data/DAL/BidDAL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bitcoin.Data.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,14 @@ using System.Threading.Tasks;
 
 namespace Bitcoin.Data.DAL
 {
-    class BidDAL
+    public class BidDAL
     {
+        readonly BitcoinEntities _bitCoinEntities = new BitcoinEntities();  
+
+        public void InsertBid (Bid bid)
+        {
+            _bitCoinEntities.Bids.Add(bid);
+            _bitCoinEntities.SaveChanges();
+        }
     }
 }
