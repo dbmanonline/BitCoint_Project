980fc04c679878bc1bd3c3cea684687658d4705d
khiemnvd <khiemnvd.dev@gmail.com>
2016-04-29 17:17:12 +0700

    update class BidDAL

--- Bitcoin.Data/DAL/BidDAL.cs
+++ Bitcoin.Data/DAL/BidDAL.cs
@@ -9,12 +9,32 @@ namespace Bitcoin.Data.DAL
 {
     public class BidDAL
     {
-        readonly BitcoinEntities _bitCoinEntities = new BitcoinEntities();  
+        readonly BitcoinEntities _bitCoinEntities = new BitcoinEntities();
 
-        public void InsertBid (Bid bid)
+        /// <summary>
+        /// Insert Bid BitCoin
+        /// </summary>
+        /// <param name="bid"> bid< /param>
+        public void InsertBid(Bid bid)
         {
             _bitCoinEntities.Bids.Add(bid);
             _bitCoinEntities.SaveChanges();
         }
+
+        /// <summary>
+        /// Get latest bid of user
+        /// </summary>
+        /// <param name="userId"> UserId of Bid </param>
+        /// <returns> Single latest bid </returns>
+        public Bid GetLatestBid(int userId)
+        {
+            var latestBid = _bitCoinEntities.Bids
+                .Where(b => b.UserID == userId)
+                .OrderByDescending(b => b.CreateDate)
+                .FirstOrDefault();
+
+            return latestBid;
+        }
+
     }
 }
