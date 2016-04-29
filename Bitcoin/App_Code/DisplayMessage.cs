359e83a3c864418114d087d1f1295f8dba2f6ed9
khiemnvd <khiemnvd.dev@gmail.com>
2016-04-29 11:26:56 +0700

    create app_code folder for common classes

---
+++ Bitcoin/App_Code/DisplayMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+/// <summary>
+/// Summary description for DisplayMessage
+/// </summary>
+public class DisplayMessage
+{
+    /// <summary>
+    /// Show a message after insert, update or delete
+    /// </summary>
+    /// <param name="content">message's content</param>
+    /// <param name="page">current page</param>
+    public static void ShowMessage(string content, Page page)
+    {
+        ScriptManager.RegisterStartupScript(page, typeof(Page), "MyScript", "alert('" + content + "');", true);
+    }
+}
