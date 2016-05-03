30a779961c24b638c6ea678265408b63e10eecfe
Khiem Nguyen <khiemnvd@hotmail.com>
2016-05-03 17:17:23 +0700

    add code PercentageBLL

---
+++ Bitcoin.Data/BLL/BidPercentageBLL.cs
@@ -0,0 +1,20 @@
+﻿using Bitcoin.Data.DAL;
+using Bitcoin.Data.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bitcoin.Data.BLL
+{
+    public class BidPercentageBLL
+    {
+        private static BidPercentageDAL _bidPercentageDAL = new BidPercentageDAL();
+
+        public BidPercentage GetCurrentPercentage()
+        {
+            return _bidPercentageDAL.GetCurrentPercentage();  
+        }
+    }
+}
