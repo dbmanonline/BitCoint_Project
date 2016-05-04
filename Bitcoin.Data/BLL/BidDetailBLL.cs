0492d9081ea3edca81ba5d6136c51a2c04259115
khiemnvd <khiemnvd.dev@gmail.com>
2016-05-04 17:31:27 +0700

    update class BidBLL and BidDetailBLL

---
+++ Bitcoin.Data/BLL/BidDetailBLL.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bitcoin.Data.DAL;
+using Bitcoin.Data.DTO;
+
+namespace Bitcoin.Data.BLL
+{
+    public class BidDetailBLL
+    {
+        private readonly BidDetailDAL _bidDetailDal = new BidDetailDAL();
+
+        public void InsertBidDetail(BidDetail bidDetail)
+        {
+            _bidDetailDal.InsertBidDetail(bidDetail);
+        }
+
+        public IEnumerable<BidDetail> GetAllBidDetails()
+        {
+            return _bidDetailDal.GetAllBidDetails();
+        }
+
+        public void UpdateBidDetail(BidDetail bidDetail)
+        {
+            _bidDetailDal.UpdateBidDetail(bidDetail);
+        }
+
+        public BidDetail GetBidDetailById(string grCode)
+        {
+            return _bidDetailDal.GetBidDetailById(grCode);
+        }
+
+        public BidDetail GetBidDetailByGPCode(string gpCode)
+        {
+            return _bidDetailDal.GetBidDetailByGPCode(gpCode);
+        }
+    }
+}
