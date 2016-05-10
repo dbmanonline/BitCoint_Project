79a12146c4b33e3b5f301592902e26cbffbc4f69
khiemnvd <khiemnvd.dev@gmail.com>
2016-05-10 17:25:32 +0700

    add bank folder

---
+++ Bitcoin/App_Code/Pagination.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+/// <summary>
+/// Pagination class
+/// </summary>
+public class Pagination
+{
+    static int _stt = 1;
+    public static void PageIndexChanging(GridView grid, GridViewPageEventArgs e)
+    {
+        grid.PageIndex = e.NewPageIndex;
+        int trangThu = e.NewPageIndex;
+        int soDong = grid.PageSize;
+
+        _stt = trangThu * soDong + 1;
+    }
+}
